041c7adbdc42225d2d7dc3099170c3d682e18896
Furkan <frknsprnl@hotmail.com>
2022-08-06 16:27:56 +0300

    filter contacts feature added

--- src/components/Contacts/List/index.js
+++ src/components/Contacts/List/index.js
@@ -1,8 +1,24 @@
+import { useState } from "react";
+
 function List({ contacts }) {
+  const [filteredText, setFilteredText] = useState("");
+
+  const filtered = contacts.filter((item) => {
+    return Object.keys(item).some((key) =>
+      item[key].toString().toLowerCase().includes(filteredText.toLowerCase())
+    );
+  });
+
   return (
     <div>
+      <input
+        placeholder="Filter Contacts"
+        value={filteredText}
+        onChange={(e) => setFilteredText(e.target.value)}
+      />
+
       <ul>
-        {contacts.map((contact, index) => (
+        {filtered.map((contact, index) => (
           <li key={index}>
             {contact.fullName} - {contact.phoneNumber}
           </li>
