313552b0f79180a37c520c9469ec225c244ebca6
Furkan <frknsprnl@hotmail.com>
2022-08-21 23:36:47 +0300

    search button resp. | theme imp.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
 import { IconButton, Switch, Text, Show, Hide } from "@chakra-ui/react";
-import { MoonIcon, SearchIcon } from "@chakra-ui/icons";
+import { MoonIcon, SearchIcon, SunIcon } from "@chakra-ui/icons";
 import "../App.css";
 import { useTheme } from "../context/themeContext";
 
 function Header() {
   const { theme, setTheme } = useTheme();
   return (
-    <header className={`header`}>
+    <header
+      className={`header`}
+      style={
+        theme === "dark"
+          ? { borderBottom: "1px solid rgba(255, 255, 255, 0.25)" }
+          : { borderBottom: "1px solid rgba(0, 0, 0, 0.25)" }
+      }
+    >
       <div className={`headerContainer`}>
         <Show below="md">
           <IconButton
@@ -14,7 +21,11 @@ function Header() {
             w={12}
             h={12}
             backgroundColor={"transparent"}
-            _hover={{ backgroundColor: "#30343c" }}
+            _hover={
+              theme === "dark"
+                ? { backgroundColor: "#30343c" }
+                : { backgroundColor: "" }
+            }
             aria-label="Search"
             icon={<SearchIcon w={8} h={8} />}
           />
@@ -23,6 +34,9 @@ function Header() {
         <Hide below="md">
           <input
             className={`searchBar ${theme === "dark" ? "light" : "dark"}`}
+            style={
+              theme === "light" ? { color: "#f3f2ef" } : { color: "#1a202c" }
+            }
             type="text"
             spellCheck={false}
           />
@@ -33,12 +47,22 @@ function Header() {
             w={12}
             h={12}
             backgroundColor={"transparent"}
-            _hover={{ backgroundColor: "#30343c" }}
+            _hover={
+              theme === "dark"
+                ? { backgroundColor: "#30343c" }
+                : { backgroundColor: "" }
+            }
             aria-label="Search database"
             onClick={() => {
               theme === "dark" ? setTheme("light") : setTheme("dark");
             }}
-            icon={<MoonIcon w={8} h={8} color={"teal"} />}
+            icon={
+              theme === "dark" ? (
+                <MoonIcon w={8} h={8} color={"teal"} />
+              ) : (
+                <SunIcon w={8} h={8} color={"teal"} />
+              )
+            }
           />
           <Text fontSize={"2xl"}>°C</Text>
           <Switch size={"lg"} colorScheme="teal" />
