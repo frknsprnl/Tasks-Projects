60d27f2899fc8e4daa0f41810c75ab4bd235f1e2
Furkan <frknsprnl@hotmail.com>
2022-08-24 23:55:00 +0300

    weather api added

--- src/api/weatherAPI.js
+++ src/api/weatherAPI.js
@@ -1,32 +1,17 @@
 import axios from "axios";
+axios.defaults.baseURL = "http://api.openweathermap.org";
 
-const getToday = () => {
-  const date = new Date();
-  let dd = String(date.getDate()).padStart(2, "0");
-  let mm = String(date.getMonth() + 1).padStart(2, "0");
-  let yy = date.getFullYear();
-
-  return `${yy}-${mm}-${dd}`;
-};
-
-const getSevenDaysLater = () => {
-  console.log(getToday());
-};
-
-getSevenDaysLater();
-
-export const getLatLon = (cityName) => {
-  axios
+export const getData = async (cityName) => {
+  const myResponse = axios
     .get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.REACT_APP_API_KEY}`
+      `/data/2.5/forecast?q=${cityName}&appid=${process.env.REACT_APP_API_KEY}`
     )
-    .then((data) => getWeatherData(data.data.coord.lat, data.data.coord.lon));
-};
-
-const getWeatherData = (lat, lon) => {
-  axios
-    .get(
-      `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m&start_date=${getToday()}&end_date=2022-08-25`
-    )
-    .then((data) => console.log(data.data));
+    .then((response) => {
+      return response.data;
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => console.log(error));
+  return myResponse;
 };
