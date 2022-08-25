744bfd601aadc14c574bc9e0099bb35bd78b39c2
Furkan <frknsprnl@hotmail.com>
2022-08-25 15:52:49 +0300

    UnitContext | UnitProvider imp.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import "./App.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { ThemeProvider } from "./context/themeContext";
 import { WeatherProvider } from "./context/weatherContext";
+import { UnitProvider } from "./context/unitContext";
 
 import Container from "./components/Container";
 function App() {
   return (
     <ThemeProvider>
       <WeatherProvider>
-        <ChakraProvider>
-          <Container />
-        </ChakraProvider>
+        <UnitProvider>
+          <ChakraProvider>
+            <Container />
+          </ChakraProvider>
+        </UnitProvider>
       </WeatherProvider>
     </ThemeProvider>
   );
---
+++ src/context/unitContext.js
@@ -0,0 +1,22 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+const UnitContext = createContext();
+
+export const UnitProvider = ({ children }) => {
+  const [unit, setUnit] = useState("C");
+
+  useEffect(() => {
+    console.log(unit);
+  }, [unit]);
+
+  const values = {
+    unit,
+    setUnit,
+  };
+
+  return (
+    <UnitContext.Provider value={values}>{children} </UnitContext.Provider>
+  );
+};
+
+export const useUnit = () => useContext(UnitContext);
