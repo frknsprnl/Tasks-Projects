c25058f6c95f6cace11bc67d596b09db4d3893be
Furkan <frknsprnl@hotmail.com>
2022-08-25 15:57:25 +0300

    Section and Sidebar added / styled

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,26 @@
 import React from "react";
 import Sidebar from "./Sidebar";
+import Section from "./Section";
+import { useTheme } from "../context/themeContext";
 
 function Main() {
+  const { theme } = useTheme();
+
   return (
-    <div>
-      <Sidebar />
+    <div style={{ display: "flex" }}>
+      <div
+        className="sideBar"
+        style={
+          theme === "dark"
+            ? { borderRight: "1px solid rgba(255, 255, 255, 0.25)" }
+            : { borderRight: "1px solid rgba(0, 0, 0, 0.25)" }
+        }
+      >
+        <Sidebar />
+      </div>
+      <div className="section">
+        <Section />
+      </div>
     </div>
   );
 }
