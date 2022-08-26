40c6b627a4ffc0537160e1b3fbc6082d82950574
Furkan <frknsprnl@hotmail.com>
2022-08-26 20:19:12 +0300

    main js clarified

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,26 +1,12 @@
 import React from "react";
 import Sidebar from "./Sidebar";
 import Section from "./Section";
-import { useTheme } from "../context/themeContext";
 
 function Main() {
-  const { theme } = useTheme();
-
   return (
-    <div style={{ display: "flex" }}>
-      <div
-        className="sideBar"
-        style={
-          theme === "dark"
-            ? { borderRight: "1px solid rgba(255, 255, 255, 0.25)" }
-            : { borderRight: "1px solid rgba(0, 0, 0, 0.25)" }
-        }
-      >
-        <Sidebar />
-      </div>
-      <div className="section">
-        <Section />
-      </div>
+    <div className="mainDiv">
+      <Sidebar />
+      <Section />
     </div>
   );
 }
