7f9632a6346f42b711451048b4e17bf0c0fbf290
Furkan <frknsprnl@hotmail.com>
2022-08-27 03:09:04 +0300

    sidebar ui fix

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -28,57 +28,61 @@ function Sidebar() {
   };
 
   return (
-    <div
-      className="sideBar"
-      style={
-        theme === "dark"
-          ? {
-              borderRight: "1px solid rgba(255, 255, 255, 0.08)",
-              borderBottom: "1px solid rgba(255, 255, 255, 0.08)",
-            }
-          : {
-              boxShadow:
-                "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",
-            }
-      }
-    >
+    <>
       {weather.name && (
-        <div style={{ marginTop: "4em" }}>
-          <Text fontWeight={600}>
-            {isoDateConverter(weather.forecast[0].date)}
-          </Text>
-          <Text style={{ fontSize: "2.5em", textAlign: "center" }}>
-            {`${weather.name}`}
-          </Text>
-          <Text
-            style={{ fontSize: "1.2em", textAlign: "center" }}
-            textColor={"gray.500"}
-          >
-            {`${weather.country}`}
-          </Text>
-          <Center paddingY={6}>
-            <Image
-              src={`https://developer.foreca.com/static/images/symbols/${weather.forecast[0].symbol}.png`}
-              alt={weather.forecast[0].symbolPhrase}
-            ></Image>
-          </Center>
-
-          <Center>
-            <Text fontSize={"1.5em"}>
-              {capitalizeFirst(weather.forecast[0].symbolPhrase)}
+        <div
+          className="sideBar"
+          style={
+            theme === "dark"
+              ? {
+                  borderRight: "1px solid rgba(255, 255, 255, 0.08)",
+                  borderBottom: "1px solid rgba(255, 255, 255, 0.08)",
+                }
+              : {
+                  boxShadow:
+                    "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",
+                }
+          }
+        >
+          <div style={{ marginTop: "4em" }}>
+            <Text fontWeight={600}>
+              {isoDateConverter(weather.forecast[0].date)}
             </Text>
-          </Center>
-
-          <Center>
-            <Text fontSize={"1.3em"}>
-              {`${weather.forecast[0].minTemp} ${
-                unit === "F" ? "°F" : "°C"
-              } / ${weather.forecast[0].maxTemp} ${unit === "F" ? "°F" : "°C"}`}
+            <Text style={{ fontSize: "2.5em", textAlign: "center" }}>
+              {`${weather.name}`}
             </Text>
-          </Center>
+            <Text
+              style={{ fontSize: "1.2em", textAlign: "center" }}
+              textColor={"gray.500"}
+            >
+              {`${weather.country}`}
+            </Text>
+            <Center paddingY={6}>
+              <Image
+                src={`https://developer.foreca.com/static/images/symbols/${weather.forecast[0].symbol}.png`}
+                alt={weather.forecast[0].symbolPhrase}
+              ></Image>
+            </Center>
+
+            <Center>
+              <Text fontSize={"1.5em"}>
+                {capitalizeFirst(weather.forecast[0].symbolPhrase)}
+              </Text>
+            </Center>
+
+            <Center>
+              <Text fontSize={"1.3em"}>
+                {`${weather.forecast[0].minTemp} ${
+                  unit === "F" ? "°F" : "°C"
+                } / ${weather.forecast[0].maxTemp} ${
+                  unit === "F" ? "°F" : "°C"
+                }`}
+              </Text>
+            </Center>
+          </div>
         </div>
       )}
-    </div>
+    </>
   );
 }
 
