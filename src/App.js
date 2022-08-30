06d57e8a47488deedc908d28d455069c57589edd
Furkan <frknsprnl@hotmail.com>
2022-08-30 23:56:38 +0300

    chakra ui font changed | montserrat

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import "./App.css";
+import "@fontsource/montserrat";
 import { ChakraProvider } from "@chakra-ui/react";
 import { ThemeProvider } from "./context/themeContext";
 import { WeatherProvider } from "./context/weatherContext";
 import { UnitProvider } from "./context/unitContext";
-
+import { extendTheme } from "@chakra-ui/react";
 import Container from "./components/Container";
+
+const theme = extendTheme({
+  fonts: {
+    body: `'Montserrat', sans-serif`,
+  },
+});
+
 function App() {
   return (
     <ThemeProvider>
       <WeatherProvider>
         <UnitProvider>
-          <ChakraProvider>
+          <ChakraProvider theme={theme}>
             <Container />
           </ChakraProvider>
         </UnitProvider>
