c9d5538ad1f1e35a94ddbf629a1cc3552f9dce78
Furkan <frknsprnl@hotmail.com>
2022-09-07 21:44:45 +0300

    Clean Blog Task 2

--- CleanBlog/backend/app.js
+++ CleanBlog/backend/app.js
@@ -1,14 +1,28 @@
 const express = require("express");
+const ejs = require("ejs");
+const path = require("path");
 
 const app = express();
 
-const port = 3000;
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../frontend/views"));
+
+app.use(express.static("../frontend/public"));
 
 app.get("/", (req, res) => {
-  const blog = { id: 1, title: "Blog title", description: "Blog description" };
-  res.send(blog);
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/add_post", (req, res) => {
+  res.render("add_post");
+});
+app.get("/post", (req, res) => {
+  res.render("post");
 });
 
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server started at port ${port}`);
 });
