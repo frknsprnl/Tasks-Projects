a107437c5950ceebf0954f43643e553f8148711a
Furkan <frknsprnl@hotmail.com>
2022-09-08 23:00:34 +0300

    Clean Blog Task 3

--- CleanBlog/backend/app.js
+++ CleanBlog/backend/app.js
@@ -1,16 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const ejs = require("ejs");
 const path = require("path");
+const Blog = require("./models/Blog");
 
 const app = express();
 
+mongoose.connect("mongodb://localhost/cleanblog-test-db");
+
+// TEMPLATE ENGINE
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../frontend/views"));
 
+// MIDDLEWARES
 app.use(express.static("../frontend/public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.render("index");
+// ROUTES
+app.get("/", async (req, res) => {
+  const blogs = await Blog.find({});
+  res.render("index", {
+    blogs,
+  });
 });
 app.get("/about", (req, res) => {
   res.render("about");
@@ -20,6 +32,11 @@ app.get("/add_post", (req, res) => {
 });
 app.get("/post", (req, res) => {
   res.render("post");
+});
+
+app.post("/blogs", async (req, res) => {
+  await Blog.create(req.body);
+  res.redirect("/");
 });
 
 const port = 3000;
