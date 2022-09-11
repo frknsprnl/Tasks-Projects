3ecccbde1a02a8363cabcc4d032e47241e37d393
Furkan <frknsprnl@hotmail.com>
2022-09-11 23:40:40 +0300

    make it better | heroku deploy

--- CleanBlog/app.js
+++ CleanBlog/app.js
@@ -4,10 +4,20 @@ const methodOverride = require("method-override");
 const ejs = require("ejs");
 const blogController = require("./controllers/blogController");
 const pageController = require("./controllers/pageController");
+require("dotenv").config();
 
 const app = express();
 
-mongoose.connect("mongodb://localhost/cleanblog-test-db");
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.HEROKU_ID}:${process.env.HEROKU_PW}@cluster0.bpaqez1.mongodb.net/cleanblog-db?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("DB Connected!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // TEMPLATE ENGINE
 app.set("view engine", "ejs");
@@ -33,7 +43,7 @@ app.post("/posts", blogController.createPost);
 app.put("/posts/:id", blogController.updatePost);
 app.delete("/posts/:id", blogController.deletePost);
 
-const port = 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server started at port ${port}`);
 });
