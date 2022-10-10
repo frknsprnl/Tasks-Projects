ea4a818e47d0719a5a9c4e7afa5506cf9bdde5a7
Furkan <frknsprnl@hotmail.com>
2022-10-10 22:34:21 +0300

    test task

--- react/EmojiSearch-Testing/src/App.test.js
+++ react/EmojiSearch-Testing/src/App.test.js
@@ -1,8 +1,42 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import {render, screen, fireEvent} from "@testing-library/react";
+import '@testing-library/jest-dom';
+
 import App from "./App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-});
+
+describe('emoji-app tests', () => {
+  let header, emojiResults;
+
+  beforeEach(() => {
+    render(<App />);
+    emojiResults = screen.getAllByTestId('component-emoji')
+
+  })
+
+// test 1 - q1
+  test('should render header', () => {
+    header = screen.getByText('Emoji Search');
+    expect(header).toBeInTheDocument();
+  })
+
+// test 2 - q2
+  test('should render emoji list', () => {
+    expect(emojiResults.length).toEqual(20);
+  })
+
+// test 3 - q3
+  test('should render emoji list by filter', () => {
+    const emoji = "100";
+    const input = screen.getByTestId('search-emoji');
+
+    fireEvent.change(input, { target: { value: emoji }});
+
+    expect(screen.getByText(emoji)).toBeInTheDocument();
+  })
+
+// test 4 - q4
+  test('should check the emoji copied to clipboard', () => {
+    expect(emojiResults[0]).toHaveAttribute('data-clipboard-text');
+  }) 
+
+})
